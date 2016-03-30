f6667ebbd4eac99119140c3e348be15475f7c582
Katie Siegel <ksiegel@mit.edu>
2016-03-30 02:53:51 +0000

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# 6MOB-project-firmware
+# 6MOB project: Raspberry Pi gateway code.
+
+## Installation instructions
+1) Clone repo onto Raspberry Pi
+2) `cd RPi`
+3) `python setup.py install`
+4) `cd RF24RaspberryCommunicator`
+5) `git clone https://github.com/edoardoo/RF24`
+6) `make clean && make`
+7) `sudo ./remote`
--- RPi/RF24RaspberryCommunicator/remote.cpp
+++ RPi/RF24RaspberryCommunicator/remote.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 RF24 radio(RPI_V2_GPIO_P1_22, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
 //const int role_pin = 7;
-const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xE8E8F0F0E1LL };
 //const uint8_t pipes[][6] = {"1Node","2Node"};
 
 // hack to avoid SEG FAULT, issue #46 on RF24 github https://github.com/TMRh20/RF24.git
@@ -31,6 +31,9 @@ void setup(void){
 	radio.openWritingPipe(pipes[0]);
 	radio.openReadingPipe(1,pipes[1]);
 	radio.startListening();
+  radio.setAutoAck(false);
+  radio.printDetails();
+
 
 }
 
@@ -46,9 +49,10 @@ void receiveMessage() {
 	if (timeout) {
 		printf("TIMEOUT\n\r");
 	} else {
-		radio.read(&got_message, sizeof("hello"));
+		radio.read(&got_message, 32);
 		printf("Got message: %s\n\r",got_message);
-		system("curl -H 'Content-Type: application/json' -X POST -d '{\"message\":\"from_rasp\"}' http://54.165.195.244:3000");
+    string combo = "curl -H 'Content-Type: application/json' -X POST -d '{\"message\":\"" + string(got_message) + "\"}' http://54.152.120.248:3000";
+		system(combo.c_str());
 	}
 }
 
@@ -56,7 +60,6 @@ int main( int argc, char ** argv){
 	setup();
 	while(true){
 		receiveMessage();
-		// sleep(1);
 	}
 	return 0;
 }
